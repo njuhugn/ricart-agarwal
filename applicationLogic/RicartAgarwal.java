f86c968ae6a89852ce1b0ed878cfd674c3bad29f
Arun Augustine <arunaugustine87@gmail.com>
2012-09-25 15:38:03 -0500

    First commit. First of many.

---
+++ applicationLogic/RicartAgarwal.java
@@ -0,0 +1,13 @@
+package applicationLogic;
+
+public class RicartAgarwal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Roger Charlie,...Initiating system start sequence\n...");
+	}
+
+}
